9fe52f019b9dee6427b782936e59b9273074ea16
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2026-03-18 04:17:17 +0300

    Implement door script

---
+++ 3D Maze Escape/Assets/Scripts/DoorController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        // We check if the object crashing into the door has the tag "Key"
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            Debug.Log("Key detected! Opening door and ending game. You Win!");
+            
+            // "Open" the door by hiding it
+            gameObject.SetActive(false);
+            
+            // End the game by stopping time
+            Time.timeScale = 0f; 
+        }
+    }
+}
