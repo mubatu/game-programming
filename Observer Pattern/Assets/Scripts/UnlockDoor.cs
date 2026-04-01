ffe4bc067c51c9eb4da00614ee258a7d409fe01d
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2026-04-01 15:20:42 +0300

    Unlock the door after enemy death

--- Observer Pattern/Assets/Scripts/UnlockDoor.cs
+++ Observer Pattern/Assets/Scripts/UnlockDoor.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class UnlockDoor : MonoBehaviour
 {
+    [SerializeField] public GameObject door;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    void OnEnable() {
+        EnemyHealth.FinalBossDeath += DestroyDoor;
+    }
+    void OnDisable() {
+        EnemyHealth.FinalBossDeath -= DestroyDoor;
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void DestroyDoor()
+    {
+        door.SetActive(false);
     }
 }
